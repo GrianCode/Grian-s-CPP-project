37593ab0ceca94789131d6ccd3793aec9e9c7dea
Grian <52743201+GrianCode@users.noreply.github.com>
2019-07-10 18:50:28 +0800

    Windows Application TEST
    Grian 's Windows Application TEST

---
+++ chery.cpp
@@ -0,0 +1,87 @@
+#include <windows.h>
+
+/* This is where all the input to the window goes to */
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+	switch(Message) {
+		
+		/* Upon destruction, tell the main thread to stop */
+		case WM_DESTROY: {
+			PostQuitMessage(0);
+			break;
+		}
+		case WM_CLOSE:
+		if (IDYES==MessageBox(hwnd,"是否真的退出？","Chery",MB_YESNO))
+		{
+			DestroyWindow(hwnd);  //销毁窗口
+		}
+		break;
+		case WM_LBUTTONDOWN:
+		MessageBox(hwnd,"鼠标已点击","Chery",0);
+		HDC hdc;
+		hdc = GetDC(hwnd);
+		TextOut(hdc, 0, 50, "哈哈哈", strlen("哈哈哈"));
+		TextOut(hdc, 0, 70, "c++真好玩", strlen("c++真好玩"));
+		ReleaseDC(hwnd, hdc);
+		break;
+		case WM_PAINT:
+		HDC hDC;
+		PAINTSTRUCT ps;
+		hDC = BeginPaint(hwnd,&ps);
+		TextOut(hDC,0,0,"HELLO,CHERY，这是本人用Dev-c++写的,点我一下！",strlen("HELLO,CHERY，这是本人用Dev-c++写的,点我一下！"));
+		EndPaint(hwnd,&ps);
+	
+	
+		/* All other messages (a lot of them) are processed using default procedures */
+		default:
+			return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+
+/* The 'main' function of Win32 GUI programs: this is where execution starts */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	WNDCLASSEX wc; /* A properties struct of our window */
+	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
+	MSG msg; /* A temporary location for all messages */
+
+	/* zero out the struct and set the stuff we want to modify */
+	memset(&wc,0,sizeof(wc));
+	wc.cbSize		 = sizeof(WNDCLASSEX);
+	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
+	wc.hInstance	 = hInstance;
+	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
+	
+	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszClassName = "WindowClass";
+	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
+	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
+
+	if(!RegisterClassEx(&wc)) {
+		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Chery",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, /* x */
+		CW_USEDEFAULT, /* y */
+		640, /* width */
+		480, /* height */
+		NULL,NULL,hInstance,NULL);
+
+	if(hwnd == NULL) {
+		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
+		return 0;
+	}
+
+	/*
+		This is the heart of our program where all input is processed and 
+		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
+		this loop will not produce unreasonably high CPU usage
+	*/
+	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
+		TranslateMessage(&msg); /* Translate key codes to chars if present */
+		DispatchMessage(&msg); /* Send it to WndProc */
+	}
+	return msg.wParam;
+}
